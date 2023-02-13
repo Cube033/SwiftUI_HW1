ed476e558a5d6eeecfecfb9cc35c579b86d17a95
Cube033 <cube033@gmail.com>
2023-02-13 09:42:05 +0100

    Initial Commit

---
+++ SwiftUI_HW1/SwiftUI_HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HW1App.swift
+//  SwiftUI_HW1
+//
+//  Created by Дмитрий Федотов on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
